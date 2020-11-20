8b89f6649f8ad5d8f8f4a24f3a112e8d92752f11
Patrick Toman <patrick.toman@uconn.edu>
2020-11-20 18:16:06 -0500

    EDA Updates

--- code/EDA.R
+++ code/EDA.R
@@ -29,7 +29,6 @@ snowfall_df$missing_ts <- 0
 snowfall_df$missing_ts[which(is.na(snowfall_df$snowf)==T)] <- 1
 
 
-
 # Plots
 snowfall_plt <- snowfall_df %>% 
   ggplot(aes(x=date,y=snowf,color=name)) +
@@ -50,9 +49,9 @@ ann_max_snowfall_plt <- annual_max_snowf_df %>%
   xlab('Date')+ylab('Maximum Snowfall (inches)')+ ggtitle('Annual Maxes')+
   facet_wrap(~name,nrow = 3,scales = 'free_y')
 
-ann_max_snowfall_plt
-missing_plt
-snowfall_plt
+#ann_max_snowfall_plt
+#missing_plt
+#snowfall_plt
 
 
 snowfall_df %>% 
@@ -68,6 +67,107 @@ date_split <- function(df){
   
 }
 
-snowdays_df %>% 
-  mutate('date_splits'=lapply(snowdays_df$data,date_split)) -> snowdays_df
 
+
+snow_events_list <- list()
+date_splits_list <- list()
+
+station_names <- snowdays_df$name
+
+for(i in 1:length(station_names)){
+  
+  df_tmp <- snowdays_df %>% 
+    filter(name == station_names[i]) 
+  
+  events_tmp <- date_split(df_tmp$data[[1]])
+  
+  df_tmp %>% 
+    unnest(cols=c(data)) -> df_temp 
+  
+  snowfall_totals_tmp <- list()
+  
+  for(j in 1:length(events_tmp)){
+    
+    df_temp %>% 
+      filter(date %in% events_tmp[[j]]) %>% 
+      summarise(snowfall_total = sum(snowf)) -> snowf_tot_tmp
+    
+    snowfall_totals_tmp[[j]] <- snowf_tot_tmp$snowfall_total
+    
+    names(snowfall_totals_tmp)[j] <- as.character(events_tmp[[j]][1])
+    
+  }
+  
+  snow_events_list[[i]] <- snowfall_totals_tmp
+  
+  date_splits_list[[i]] <- events_tmp
+  
+  names(snow_events_list)[i] <- names(date_splits_list)[i] <- station_names[i]
+  
+}
+
+#########################################################################
+# Required Data based on authors data file layout 
+
+# Annual maxes 
+
+annmax_midway <- annual_max_snowf_df %>% 
+  filter(name=='Midway') %>% 
+  select(ann_max_snowf)
+
+annmax_ohare <- annual_max_snowf_df %>% 
+  filter(name=='Ohare') %>% 
+  select(ann_max_snowf)
+
+annmax_parkforest <- annual_max_snowf_df %>% 
+  filter(name=='ParkForest') %>% 
+  select(ann_max_snowf)
+
+# Indices of days where snowfall events occur 
+
+days_midway <- snowfall_df %>%
+  filter(name=='Midway') 
+
+days_midway <- which(days_midway$date %in% ymd(names(snow_events_list$Midway)))
+
+days_ohare <- snowfall_df %>%
+  filter(name=='Ohare') 
+
+days_ohare <- which(days_ohare$date %in% ymd(names(snow_events_list$Ohare)))
+
+days_parkforest <- snowfall_df %>%
+  filter(name=='ParkForest') 
+
+days_parkforest <- which(days_parkforest$date %in% ymd(names(snow_events_list$ParkForest)))
+
+# Durations of snowevents 
+
+duration_midway <- unlist(lapply(date_splits_list$Midway, length))
+
+duration_ohare <- unlist(lapply(date_splits_list$Ohare, length))
+
+duration_parkforest <- unlist(lapply(date_splits_list$ParkForest, length))
+
+# Snow event values 
+nonzero_snow_midway <- unlist(snow_events_list$Midway)
+
+nonzero_snow_ohare <- unlist(snow_events_list$Ohare)
+
+nonzero_snow_parkforest <- unlist(snow_events_list$ParkForest)
+
+# Season 
+# Need to define intervals for this - recall a snow year runs from July 01 to June 30
+date_seq <- seq.Date(from = as.Date('1960-07-01'),to =as.Date('2020-06-30'),by='year')
+
+make_season <- function(df,year_start=1960,year_end=2020){
+
+  date_seq <- seq.Date(from = as.Date('1960-07-01'),to =as.Date('2020-06-30'),by='year')
+    
+  nseasons <- length(date_seq)
+  
+  snow_year_start <- '07-01'
+  
+  snow_year_end <- '06-30'
+  
+  
+}
