0a09b8deefe240fbb730a940cf97f02845fca8b4
Patrick Toman <patrick.toman@uconn.edu>
2020-11-20 16:24:53 -0500

    Update EDA code

--- code/EDA.R
+++ code/EDA.R
@@ -17,20 +17,32 @@ snowfall_df %>%
 
 
 snowfall_df %>% 
-  group_by(station,name) %>% 
+  group_by(station,name,month(date)) %>% 
   summarise(ct = n(),snowf_missing = sum(is.na(snowf)),snowf_pct_missing = sum(is.na(snowf))/n(),
             snowdep_missing = sum(is.na(snowdep)),snowdep_pct_missing = sum(is.na(snowdep))/n()) -> missingness_df 
-
 snowfall_df %>% 
   group_by(station,name,'year'=year(date)) %>% 
   summarise(ann_max_snowf = max(snowf,na.rm = T)) -> annual_max_snowf_df 
 
-# Plots 
+snowfall_df$missing_ts <- 0
+
+snowfall_df$missing_ts[which(is.na(snowfall_df$snowf)==T)] <- 1
+
+
+
+# Plots
 snowfall_plt <- snowfall_df %>% 
   ggplot(aes(x=date,y=snowf,color=name)) +
   geom_line() + theme_minimal() + theme(legend.position = 'none',axis.text.x = element_text(angle = 45,hjust = 1))+
   scale_x_date(date_breaks = '5 years') +xlab('Date')+ylab('Snowfall (inches)') + 
   ggtitle('Daily Snowfall')+ facet_wrap(~name,nrow = 3,scales = 'free_y')
+
+missing_plt <- snowfall_df %>% 
+  ggplot(aes(x=date,y=missing_ts,color=name)) +
+  geom_line() + theme_minimal() + theme(legend.position = 'none',axis.text.x = element_text(angle = 45,hjust = 1))+
+  scale_x_date(date_breaks = '5 years') +xlab('Date')+ylab('Missing Indicator') + 
+  ggtitle('Missingness Plots')+ facet_wrap(~name,nrow = 3,scales = 'free_y')
+
 
 ann_max_snowfall_plt <- annual_max_snowf_df %>% 
   ggplot(aes(x=as.numeric(year),y=ann_max_snowf,color=name)) +
@@ -39,7 +51,7 @@ ann_max_snowfall_plt <- annual_max_snowf_df %>%
   facet_wrap(~name,nrow = 3,scales = 'free_y')
 
 ann_max_snowfall_plt
-
+missing_plt
 snowfall_plt
 
 
@@ -56,4 +68,6 @@ date_split <- function(df){
   
 }
 
+snowdays_df %>% 
+  mutate('date_splits'=lapply(snowdays_df$data,date_split)) -> snowdays_df
 
